7ea2bd1a60f1788b809a4be1dda7412952242c04
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-26 10:06:00 -0300

    setup(config): implemented setup configuration and dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 80
+  },
+  preview: {
+    port: 80
+  }
 })
