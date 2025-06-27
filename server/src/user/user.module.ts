b4145f4b307a598fa6455be65fa7472820e251e7
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-26 22:52:34 -0300

    feat(prisma): included prisma to imports

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,6 +1,6 @@
+import { PrismaModule } from '../prisma/prisma.module';
 import { UserController } from './user.controller';
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
