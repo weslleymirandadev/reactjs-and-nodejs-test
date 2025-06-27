cfc21d7f6631b03b31f05abc114a138a7aa9d64e
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-27 01:22:57 -0300

    feat(modules): included meeting module on it

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MeetingModule } from './meeting/meeting.module';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  }), PrismaModule, UserModule, AuthModule],
+  }), PrismaModule, UserModule, AuthModule, MeetingModule],
 })
 export class AppModule {}
