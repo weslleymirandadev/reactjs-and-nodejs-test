f692980c7dd8d3638d0417ed4fd745f07d4678e7
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-26 22:57:18 -0300

    feat(CORS): successfuly implemented

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "http://localhost",
+      "http://127.0.0.1",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
+    credentials: true,
+    preflightContinue: false,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(8080);
 }
 bootstrap();
