d9e631f5316630f1f7072c04fa974556c4cd836f
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-27 02:06:16 -0300

    feat(swagger): added api documentation

--- server/src/meeting/dto/update-meeting.dto.ts
+++ server/src/meeting/dto/update-meeting.dto.ts
@@ -1,16 +1,20 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsDateString, IsOptional } from "class-validator";
 
 export class UpdateMeetingDto {
+    @ApiPropertyOptional({ example: 'Weekly Sync' })
     @IsOptional()
     @IsString()
     @IsNotEmpty()
     title?: string;
 
+    @ApiPropertyOptional({ example: 'Discuss project updates' })
     @IsOptional()
     @IsString()
     @IsNotEmpty()
     description?: string;
 
+    @ApiPropertyOptional({ example: '2025-07-01T15:00:00.000Z' })
     @IsOptional()
     @IsDateString()
     date?: string;
