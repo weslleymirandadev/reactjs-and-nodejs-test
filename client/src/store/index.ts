9eabee589cfd23beb21cb29c66303892a32f96f6
weslleymirandadev <contato@weslleymiranda.dev>
2025-06-27 01:14:41 -0300

    feat(wallet): added wallet reducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import etherTooltipReducer from './etherTooltipSlice';
+import walletReducer from './walletSlice';
 
 export const store = configureStore({
   reducer: {
     etherTooltip: etherTooltipReducer,
+    wallet: walletReducer,
   },
 });
 
